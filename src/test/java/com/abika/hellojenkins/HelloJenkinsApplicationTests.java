32fffa182bbbedcf1132146c08e745197517b5f6
Abikananda <abikananda.2012@gmail.com>
2022-01-26 20:14:36 +0530

    first commit

---
+++ src/test/java/com/abika/hellojenkins/HelloJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abika.hellojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
